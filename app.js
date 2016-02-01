778c4dce092249822f7af950eeec2f637751a194
Chip Bell <cbell@pbs.org>
2016-02-01 10:23:28 -0500

    Playing with some parameters

--- app.js
+++ app.js
@@ -17,15 +17,21 @@ var graphics = PIXI.Sprite.fromImage('checker.png');
 graphics.anchor.x = graphics.anchor.y = 0.5;
 graphics.position.x = viewWidth / 2;
 graphics.position.y = viewHeight / 2;
+graphics.width = graphics.height = 300;
 graphics.filters = [
-  new PerspectiveTransform({ x_scale: 2 })
+  new PerspectiveTransform({
+    x_scale: 1,
+    y_scale: 1,
+    x_focus: 0,
+    y_focus: 0,
+  })
 ];
 stage.addChild(graphics);
 
 requestAnimationFrame(animate);
 
 function animate() {
-  graphics.rotation += 0.01;
+  //graphics.rotation += 0.01;
   renderer.render(stage);
   requestAnimationFrame(animate);
 }
