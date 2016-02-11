00d41278a7a4efe701fd5fa41c757fefcf3906bc
Chip Bell <cbell@pbs.org>
2016-02-10 21:25:34 -0500

    Tinkering with the shader

--- PerspectiveTransform.js
+++ PerspectiveTransform.js
@@ -1,3 +1,7 @@
+/**
+ * Maps a "perspective" transform to a sprite by taking a center point and focal point and pointing the focal point
+ * "north" (the negative y direction). The center point is treated as (0.5, 0.5) and 
+ */
 var PerspectiveTransform = function(options) {
   var defaults = {
     x_focus: 0.5,
@@ -27,9 +31,27 @@ var PerspectiveTransform = function(options) {
     uniformsDeclaration,
 
     'void main(void) {',
-    '  vec2 uv = vTextureCoord.xy;',
-    '  uv.x = (uv.x - x_focus) / (uv.y - y_focus) / x_scale + x_focus;',
-    '  gl_FragColor = texture2D(uSampler, uv);',
+    '  vec3 uv = vec3(vTextureCoord.xy, 1.0);',
+    // apply transformation to "undo" a perspective
+    
+    // unscale
+    '  mat3 unscale = mat3(vec3(1.0, 0.0, 0.0), vec3(0.0, 1.0, 0.0), vec3(0.0, 0.0, 1.0));',
+    '  uv = unscale * uv;',
+   
+    // "unperspective"
+    //'  uv.x = uv.x / (1.0 - uv.y);',
+
+    // unrotate
+    '  float theta = 3.14159 / 4.0;',
+    '  mat3 unrotate = mat3( vec3(cos(theta), sin(theta), 0.0), vec3(-sin(theta), cos(theta), 0.0), vec3(0.0, 0.0, 1.0));',
+    '  uv = unrotate * uv;',
+
+    // untranslate
+    '  uv.x +=  0.3;',
+    '  uv.y += -0.2;',
+
+    '  gl_FragColor = texture2D(uSampler, uv.xy);',
+    '  if(uv.x < 0.0 || uv.y < 0.0 || uv.x > 1.0 || uv.y > 1.0) gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);',
     '}',
 
   ].join('\n');
