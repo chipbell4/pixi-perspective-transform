5a044fa652912599e25ce11133fd7d1db4e609fb
Chip Bell <cbell@pbs.org>
2016-07-05 11:01:45 -0400

    Pulling in the default vertex shader

--- PerspectiveTransform.js
+++ PerspectiveTransform.js
@@ -18,9 +18,27 @@
  * @param {float} options.viewport_height The height of the viewport
  */
 var PerspectiveTransform = function(options) {
+  var vertexShader = [
+    'precision lowp float;',
+    'attribute vec2 aVertexPosition;',
+    'attribute vec2 aTextureCoord;',
+    'attribute vec4 aColor;',
+
+    'uniform mat3 projectionMatrix;',
+
+    'varying vec2 vTextureCoord;',
+    'varying vec4 vColor;',
+
+    'void main(void){',
+    '   gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);',
+    '   vTextureCoord = aTextureCoord;',
+    '   vColor = vec4(aColor.rgb * aColor.a, aColor.a);',
+    '}'
+  ].join('\n');
+
   PIXI.AbstractFilter.call(
     this,
-    null,
+    vertexShader,
     null,
     {}
   );
