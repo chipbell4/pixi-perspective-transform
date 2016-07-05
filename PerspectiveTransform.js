1b01430f0a69675d71e111f517c545cedd737080
Chip Bell <cbell@pbs.org>
2016-07-05 11:35:13 -0400

    updating comments

--- PerspectiveTransform.js
+++ PerspectiveTransform.js
@@ -1,21 +1,10 @@
 /**
- * Maps a "perspective" transform to a sprite by taking a center point and focal point and pointing the focal point
- * north, treating the center point as the origin. The focal point is also treated as the "center of perspective",
- * so that points on the image "collapse" to that very point. We also apply an "x scale" perpendicular to the
- * perspective access. Lastly, the size of the sprite and viewport are needed to fix scaling issues associated with
- * PIXI's inner texture coordinate representation, which is relative to the viewport and not the sprite. In my case,
- * I'm expecting the points to be in the "sprite unit square" ranging from (0,0) to (1,1), pointing within the size of
- * the sprite
+ * Applies a mode-7 style perspective transform, by pinching along the x-axis, based on the y coordinate. This makes it
+ * skinnier at the top (or bottom), and wider at the bottom (or top), making the texture look like a trapezoid
  *
- * @param {float} options.x_scale The x scale factor to use
- * @param {float} options.c_x The x position of the center vector
- * @param {float} options.c_y The y position of the center vector
- * @param {float} options.f_radius The distance f is from c
- * @param {float} options.f_theta The angle between f and c
- * @param {float} options.sprite_width The width of the sprite
- * @param {float} options.sprite_height The height of the sprite
- * @param {float} options.viewport_width The width of the viewport
- * @param {float} options.viewport_height The height of the viewport
+ * @param {float} options.bottom_scale The scale to use at the bottom
+ * @param {float} options.top_scale The scale to use at the top
+ * @param {float} options.scale_x_center The center from which to scale
  */
 var PerspectiveTransform = function(options) {
   var vertexShader = [
