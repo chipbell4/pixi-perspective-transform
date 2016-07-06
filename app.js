853e75667f6805f5bb844caec8cccd11ae46059c
Chip Bell <cbell@pbs.org>
2016-07-06 01:15:15 -0400

    Comments

--- app.js
+++ app.js
@@ -10,30 +10,33 @@ document.body.appendChild(renderer.view);
 // create an new instance of a pixi stage
 var stage = new PIXI.Stage(0xFFFFFF);
 
+// create the sprite
 var sprite = PIXI.Sprite.fromImage('checker.png');
 sprite.width = renderer.width * 0.5;
 sprite.height = renderer.height * 0.5;
 sprite.position.x = renderer.width * 0.125;
 sprite.position.y = renderer.height * 0.125;
+stage.addChild(sprite);
 
+// add the filter
 var perspective = new PerspectiveTransform({
   viewport_dimensions: [renderer.width, renderer.height],
   sprite_dimensions: [sprite.width, sprite.height],
 });
 sprite.filters = [perspective];
-stage.addChild(sprite);
 
+// expose some globals for testing
 window.sprite = sprite;
 window.perspective = perspective;
 
-requestAnimationFrame(animate);
-
+// start the animation loop
 function animate() {
   renderer.render(stage);
   requestAnimationFrame(animate);
 }
+requestAnimationFrame(animate);
 
-// Allow the range to control values
+// hook up the range elements to the uniforms on the shader
 var keys = ['bottom_scale', 'top_scale', 'scale_x_center'];
 keys.forEach(function(key) {
   var range = document.getElementById(key);
